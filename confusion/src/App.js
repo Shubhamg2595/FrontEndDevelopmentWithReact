84003901d1630ab57585f19f1e87c770c31268ee
Shubham Gupta <shubhamg2595@gmail.com>
2019-01-21 17:04:47 +0530

    great use of state and props

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent'
-
+import {DISHES} from './shared/dishes'
 
 class App extends Component {
+
+  constructor(props)
+  {
+    super(props);
+    this.state={
+      dishes:DISHES
+    }
+  }
+
   render() {
     return (
-      <div className="App">
+      <div >
         <Navbar light color="primary">
           <div className="container">
             <NavbarBrand href="/">Restaurant Confusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
