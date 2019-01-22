d8aee8e3dc7bbd2d948306eae8e08bcd76c665e1
Shubham Gupta <shubhamg2595@gmail.com>
2019-01-22 20:06:20 +0530

    Presentational and container components

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,28 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent'
-import {DISHES} from './shared/dishes'
+import Main from './components/MainComponent'
 
 class App extends Component {
-
-  constructor(props)
-  {
-    super(props);
-    this.state={
-      dishes:DISHES
-    }
-  }
-
   render() {
     return (
       <div >
-        <Navbar light color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Restaurant Confusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
+        <Main />
       </div>
     );
   }
--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -16,7 +16,6 @@ class DishDetail extends Component
                     <li key={comment.id} >
                         <p>{comment.comment}</p>
                         <p>--{comment.author}
-                        <br />
                         &nbsp;
                         {new Intl.DateTimeFormat('en-US', {
                             year: 'numeric',
@@ -88,17 +87,3 @@ class DishDetail extends Component
 
 
 export default DishDetail;
-
-{/*
-Task 1
-
-In this task you will be adding a new DishdetailComponent to your React application and include the component into the menu component's view so that the details of a specific dish are displayed there:
-
-    Replace the card showing the selected dish in MenuComponent's view with the DishdetailComponent, and make sure to pass the selected dish information as props to the DishdetailComponent.
-    Create a new DishDetail class in a file named DishdetailComponent.js in the components folder
-    Export the DishDetail class from this file so that it can be imported in MenuComponent.js and used to construct the view of the selected dish.
-    Return a <div> from the render() function. This <div> should use the Bootstrap row class to position the content within the <div>. This div will display both the details of the dish in a Card and the list of comments side-by-side for medium to extra large screens, but will stack them for xs and sm screens.
-    The card should be enclosed inside a <div> appropriate Bootstrap column classes so that it occupies the entire 12 columns for the xs and sm screen sizes, and 5 columns for md screens and above. Also apply a class of m-1 to this div.
-    The comments should be enclosed in a <div> to which you apply appropriate column classes so that it occupies the entire 12 columns for the xs and sm screen sizes, and 5 columns for md screens and above. Also apply a class of m-1 to this div.
-    If the dish is null then you should return an empty <div>
-*/}
